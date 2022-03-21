4498bf66f6ca8ad5b5f8148fe1a953939a7f1797
KlausGrosser <klaus.grosser@deliveryhero.com>
2022-03-21 13:27:06 +0100

    First Commit

---
+++ src/main/java/com/example/amzdeploydemo/AmzDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amzdeploydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzDeployDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmzDeployDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amzdeploydemo/AmzDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amzdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmzDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
